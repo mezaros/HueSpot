1e5218829595e28f1b2d9a69689f157caebc5295
Mike Mezaros <mezaros@gmail.com>
2026-02-24 00:17:47 -0500

    Add source copyright headers and keep LICENSE.txt only

--- HueSpot/HUDView.swift
+++ HueSpot/HUDView.swift
@@ -1,3 +1,4 @@
+// Copyright © 2026 Mark Zaros. All Rights Reserved. License: GNU Public License 2.0 only.
 import SwiftUI
 import AppKit
 
--- HueSpot/WikipediaColorData.swift
+++ HueSpot/WikipediaColorData.swift
@@ -1,3 +1,4 @@
+// Copyright © 2026 Mark Zaros. All Rights Reserved. License: GNU Public License 2.0 only.
 enum WikipediaColorData {
     static let rows: [(hex: String, name: String)] = [
         ("#0048BA", "Absolute Zero"),
